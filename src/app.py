3b2fc1c6fc217c1f7978f831186392b1535760fc
Hussam Atoui <hussam.atoui@outlook.com>
2025-02-27 13:10:45 +0100

    TASK: Modify user application

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import numpy as np
 import joblib
 import os
 import json
+import pickle
 
 # Set the working directory to src/
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -18,33 +19,45 @@ model = joblib.load('best_model.pkl')
 with open('top_features.json', 'r') as f:
     top_features = json.load(f)
 
+# Load feature ranges from the JSON file
+with open("feature_ranges.json", "r") as json_file:
+    feature_ranges = json.load(json_file)
+
+# Load feature and target scalers
+scaler = pickle.load(open("scaler.pkl", "rb"))
+target_scaler = pickle.load(open("target_scaler.pkl", "rb"))
+
 # Route for the homepage
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', features=top_features)
 
 # Route for predictions
 @app.route('/predict', methods=['POST'])
 def predict():
     # Extract data from the form
     try:
-        # Extract the input values for the top 5 features dynamically
-        features = []
-        for feature in top_features.keys():  # Use the top feature names dynamically
-            features.append(float(request.form[feature]))
+        # Get user inputs as floats and store them in a dictionary
+        user_inputs = {
+            feature: float(request.form[feature]) for feature in feature_ranges.keys()
+        }
         
         # Convert the data into a format that the model can work with (2D array)
-        feature_array = np.array([features])
+        features = list(user_inputs.values())
+        feature_array = np.array([features]).reshape(1, -1)
+        features_scaled = scaler.transform(feature_array)
 
         # Predict the house price
-        prediction = model.predict(features)[0]
+        prediction_scaled = model.predict(features_scaled)[0]
+        prediction = target_scaler.inverse_transform([[prediction_scaled]])[0][0]
+
 
         # Return the predicted house price
-        return render_template('index.html', prediction_text=f"Predicted House Price: ${prediction:,.2f}", features=top_features)
+        return render_template('index.html', prediction_text= float(prediction), feature_ranges=feature_ranges, user_inputs=user_inputs)
     
     except Exception as e:
         # If there's an error, return it in the response
-        return render_template('index.html', prediction_text=f"Error: {str(e)}", features=top_features)
+        return render_template('index.html', prediction_text=f"Error: {str(e)}", feature_ranges=feature_ranges, user_inputs=user_inputs)
 
 if __name__ == "__main__":
     app.run(debug=True)
